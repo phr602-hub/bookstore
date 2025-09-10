b7b5cc4bd2daf4ddd5e4c989af9f0596b0c41aef
phr602 <rimirimirimi040602@gmail.com>
2025-09-10 18:07:53 +0900

    massge0910

---
+++ js/book.js
@@ -0,0 +1,348 @@
+// 지금 많이 읽고 있는 작품
+    async function bookData() {
+    const REST_API_KEY = 'fdad087a1d6a396138baab34ca701d7f';
+    const params = new URLSearchParams({
+        target: 'person',
+        query: "김유신"
+    });
+
+    const url = `https://dapi.kakao.com/v3/search/book?${params}`;
+
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                Authorization: `KakaoAK ${REST_API_KEY}`
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP 오류! 상태 코드: ${response.status}`);
+        }
+
+        const data = await response.json();
+        
+        // .box 요소 전체 선택
+        const boxElements = document.querySelectorAll(".section .swiper-slide");
+        console.log(boxElements)
+
+        // documents 데이터를 각 box에 대응하여 렌더링
+        boxElements.forEach((box, i) => {
+            const doc = data.documents[i];
+
+            if (!doc) return; // 데이터가 부족할 경우 생략
+
+            // <img>
+            const img = document.createElement("img");
+            img.src = doc.thumbnail;
+            box.appendChild(img);
+
+            // <h3> 제목
+            const h3 = document.createElement("h3");
+            h3.textContent = doc.title.substring(0,5);
+            box.appendChild(h3);
+
+            // <h6> 저자
+            const h6 = document.createElement("h6");
+            h6.textContent = doc.authors;
+            box.appendChild(h6);
+
+            // <p> 내용 일부
+            const p = document.createElement("p");
+            p.textContent = doc.contents.substring(0, 60);
+            box.appendChild(p);
+
+            // <button>
+            const btn = document.createElement("button");
+            btn.textContent = "click";
+            box.appendChild(btn);
+        });
+
+
+
+    } catch (error) {
+        console.log('에러발생', error);
+    }
+}
+bookData();
+
+
+
+// 오늘, 리디의 발견
+async function bookData2() {
+    const REST_API_KEY = 'fdad087a1d6a396138baab34ca701d7f';
+    const params = new URLSearchParams({
+        target: 'person',
+        query: "정유정"
+    });
+
+    const url = `https://dapi.kakao.com/v3/search/book?${params}`;
+
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                Authorization: `KakaoAK ${REST_API_KEY}`
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP 오류! 상태 코드: ${response.status}`);
+        }
+
+        const data = await response.json();
+        
+        // .box 요소 전체 선택
+        const boxElements = document.querySelectorAll(".section2 .swiper-slide");
+        console.log(boxElements)
+
+        // documents 데이터를 각 box에 대응하여 렌더링
+        boxElements.forEach((box, i) => {
+            const doc = data.documents[i];
+
+            if (!doc) return; // 데이터가 부족할 경우 생략
+
+            // <img>
+            const img = document.createElement("img");
+            img.src = doc.thumbnail;
+            box.appendChild(img);
+
+            // <h3> 제목
+            const h3 = document.createElement("h3");
+            h3.textContent = doc.title.substring(0,5);
+            box.appendChild(h3);
+
+            // <h6> 저자
+            const h6 = document.createElement("h6");
+            h6.textContent = doc.authors;
+            box.appendChild(h6);
+
+            // <p> 내용 일부
+            const p = document.createElement("p");
+            p.textContent = doc.contents.substring(0, 60);
+            box.appendChild(p);
+
+            // <button>
+            const btn = document.createElement("button");
+            btn.textContent = "click";
+            box.appendChild(btn);
+        });
+
+
+
+    } catch (error) {
+        console.log('에러발생', error);
+    }
+}
+bookData2();
+
+
+
+
+// 베스트
+async function bookData3() {
+    const REST_API_KEY = 'fdad087a1d6a396138baab34ca701d7f';
+    const params = new URLSearchParams({
+        target: 'person',
+        query: "이상"
+    });
+
+    const url = `https://dapi.kakao.com/v3/search/book?${params}`;
+
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                Authorization: `KakaoAK ${REST_API_KEY}`
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP 오류! 상태 코드: ${response.status}`);
+        }
+
+        const data = await response.json();
+        
+        // .box 요소 전체 선택
+        const boxElements = document.querySelectorAll(".section3 .swiper-slide");
+        console.log(boxElements)
+
+        // documents 데이터를 각 box에 대응하여 렌더링
+        boxElements.forEach((box, i) => {
+            const doc = data.documents[i];
+
+            if (!doc) return; // 데이터가 부족할 경우 생략
+
+            // <img>
+            const img = document.createElement("img");
+            img.src = doc.thumbnail;
+            box.appendChild(img);
+
+            // <h3> 제목
+            const h3 = document.createElement("h3");
+            h3.textContent = doc.title.substring(0,5);
+            box.appendChild(h3);
+
+            // <h6> 저자
+            const h6 = document.createElement("h6");
+            h6.textContent = doc.authors;
+            box.appendChild(h6);
+
+            // <p> 내용 일부
+            const p = document.createElement("p");
+            p.textContent = doc.contents.substring(0, 60);
+            box.appendChild(p);
+
+            // <button>
+            const btn = document.createElement("button");
+            btn.textContent = "click";
+            box.appendChild(btn);
+        });
+
+
+
+    } catch (error) {
+        console.log('에러발생', error);
+    }
+}
+bookData3();
+
+
+
+// 지금, 리디에서만! 선 출간 신작
+async function bookData5() {
+    const REST_API_KEY = 'fdad087a1d6a396138baab34ca701d7f';
+    const params = new URLSearchParams({
+        target: 'person',
+        query: "한강"
+    });
+
+    const url = `https://dapi.kakao.com/v3/search/book?${params}`;
+
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                Authorization: `KakaoAK ${REST_API_KEY}`
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP 오류! 상태 코드: ${response.status}`);
+        }
+
+        const data = await response.json();
+        
+        // .box 요소 전체 선택
+        const boxElements = document.querySelectorAll(".section4 .swiper-slide");
+        console.log(boxElements)
+
+        // documents 데이터를 각 box에 대응하여 렌더링
+        boxElements.forEach((box, i) => {
+            const doc = data.documents[i];
+
+            if (!doc) return; // 데이터가 부족할 경우 생략
+
+            // <img>
+            const img = document.createElement("img");
+            img.src = doc.thumbnail;
+            box.appendChild(img);
+
+            // <h3> 제목
+            const h3 = document.createElement("h3");
+            h3.textContent = doc.title.substring(0,5);
+            box.appendChild(h3);
+
+            // <h6> 저자
+            const h6 = document.createElement("h6");
+            h6.textContent = doc.authors;
+            box.appendChild(h6);
+
+            // <p> 내용 일부
+            const p = document.createElement("p");
+            p.textContent = doc.contents.substring(0, 60);
+            box.appendChild(p);
+
+            // <button>
+            const btn = document.createElement("button");
+            btn.textContent = "click";
+            box.appendChild(btn);
+        });
+
+
+
+    } catch (error) {
+        console.log('에러발생', error);
+    }
+}
+bookData5();
+
+
+
+// 새로나온 작품
+async function bookData6() {
+    const REST_API_KEY = 'fdad087a1d6a396138baab34ca701d7f';
+    const params = new URLSearchParams({
+        target: 'person',
+        query: "장강명"
+    });
+
+    const url = `https://dapi.kakao.com/v3/search/book?${params}`;
+
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                Authorization: `KakaoAK ${REST_API_KEY}`
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP 오류! 상태 코드: ${response.status}`);
+        }
+
+        const data = await response.json();
+        
+        // .box 요소 전체 선택
+        const boxElements = document.querySelectorAll(".section5 .swiper-slide");
+        console.log(boxElements)
+
+        // documents 데이터를 각 box에 대응하여 렌더링
+        boxElements.forEach((box, i) => {
+            const doc = data.documents[i];
+
+            if (!doc) return; // 데이터가 부족할 경우 생략
+
+            // <img>
+            const img = document.createElement("img");
+            img.src = doc.thumbnail;
+            box.appendChild(img);
+
+            // <h3> 제목
+            const h3 = document.createElement("h3");
+            h3.textContent = doc.title.substring(0,5);
+            box.appendChild(h3);
+
+            // <h6> 저자
+            const h6 = document.createElement("h6");
+            h6.textContent = doc.authors;
+            box.appendChild(h6);
+
+            // <p> 내용 일부
+            const p = document.createElement("p");
+            p.textContent = doc.contents.substring(0, 60);
+            box.appendChild(p);
+
+            // <button>
+            const btn = document.createElement("button");
+            btn.textContent = "click";
+            box.appendChild(btn);
+        });
+
+
+
+    } catch (error) {
+        console.log('에러발생', error);
+    }
+}
+bookData6();
